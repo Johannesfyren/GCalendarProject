f084a525d4ace28376ad05f4fb23acc9ffe82f72
Johannes Mac <159946573+Johannesfyren@users.noreply.github.com>
2024-06-23 07:42:55 +0200

    Add Gapi integration with OAUTH

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import * as css from "./style.css";
 import codeEx from "./codex.js"
+const calendarName = document.querySelector("#cal-name");
+const btnReserve = document.querySelector("#btn-reserve");
+const meetingTitle = document.querySelector("#meet-title");
+const meetingTime = document.querySelector("#meet-time");
 
-console.log(codeEx);
+
+const startTime = new Date(codeEx.codeEx.start.dateTime);
+const endTime = new Date(codeEx.codeEx.end.dateTime);
+
+meetingTitle.textContent = codeEx.codeEx.summary;
+
+console.log(startTime.getHours());
+console.log(endTime.getHours());
+console.log(codeEx.codeEx.start.dateTime);
 
