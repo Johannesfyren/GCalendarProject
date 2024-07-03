06b1c2a0b372604b97bffeb3967c8a22caf8ee24
Johannes Mac <159946573+Johannesfyren@users.noreply.github.com>
2024-07-03 22:08:02 +0200

    Modify WebPack single entry instead of multi

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ const path = require('path');
 module.exports = {
   entry: {
     index: './src/index.js',
-    codeexample: './src/codex.js',
-    gapi: './src/gapi.js',
-    stateController: './src/stateController.js',
-    countdown:'./src/countDown.js'
+    // gapi: './src/gapi.js',
+    // stateController: './src/stateController.js',
+    // countdown:'./src/countDown.js',
+    // manMeetings:'./src/manipulateMeetings.js',
   },
   mode: 'development',
   devtool: 'inline-source-map',
