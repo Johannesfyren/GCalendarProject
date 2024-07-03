7f36d522213c761203143f7ac6a644ecac096be2
Johannes Mac <159946573+Johannesfyren@users.noreply.github.com>
2024-07-03 22:07:19 +0200

    Add dialog and styling

--- src/manipulateMeetings.js
+++ src/manipulateMeetings.js
@@ -1,8 +1,18 @@
 import { createEvent } from "./gapi";
 
 const reserve = document.querySelector('#btn-reserve');
+const reserveDialog = document.querySelector('#reserve-dialog');
+const closeReserveDialog = document.querySelector('#reserve-close');
+
 
 reserve.addEventListener('click', ()=>{
-    createEvent(30);
+    //createEvent(30);
     console.log("dad");
+    reserveDialog.showModal();
+})
+
+closeReserveDialog.addEventListener('click', ()=>{
+    //createEvent(30);
+    console.log("dad");
+    reserveDialog.close();
 })
