6bff053c4abe9993256c7082448c6904732489de
Johannes Mac <159946573+Johannesfyren@users.noreply.github.com>
2024-09-21 13:04:05 +0200

    testingbuild

--- src/index.js
+++ src/index.js
@@ -12,7 +12,14 @@ import bgImgOccupied from './img/Bg-img-occupied.png';
 
 
 
-
+async function authenticate() {
+    const response = await fetch('/auth');
+    const message = await response.text();
+    console.log(message); // Log the message from the server
+  }
+  
+  // Call the authenticate function when needed (e.g., on a button click)
+  document.querySelector('.test-auth').addEventListener('click', authenticate);
 
 
 
